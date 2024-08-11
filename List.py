9d152546f973a26bdc6a5ea68ac874205e497646
dylancsu <dylan.dean@csuglobal.edu>
2024-08-11 14:54:28 -0600

    Fix function signature

--- List.py
+++ List.py
@@ -3,7 +3,7 @@ class ItemToPurchase:
         self.item_name=kwargs.get('item_name','none')
         self.item_price=kwargs.get('item_price',0.0)
         self.item_quantity=kwargs.get('item_quantity',0)
-    def print_item_cost():
+    def print_item_cost(self):
         print(f'{self.item_name} {self.item_quantity} @ ${self.item_cost} = ${self.item_cost * self.item_quantity}')
 
 items=[]
