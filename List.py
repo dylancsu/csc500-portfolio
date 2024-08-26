407fe404c5b717cdb844a5230f5c67daebf54710
dylancsu <dylan.dean@csuglobal.edu>
2024-08-25 19:19:47 -0600

    Remove example from List

--- List.py
+++ List.py
@@ -6,18 +6,3 @@ class ItemToPurchase:
         self.item_quantity=kwargs.get('item_quantity',0)
     def print_item_cost(self):
         print(f'{self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${(self.item_price * self.item_quantity):.2f}')
-
-items=[]
-for i in range(1,3):
-    print(f"Item {i}")
-    name = input("Enter the item name:\n")
-    price = float(input("Enter the item price:\n"))
-    quantity = int(input("Enter the item quantity:\n"))
-    new_item=ItemToPurchase(item_price=price,item_name=name,item_quantity=quantity)
-    items.append(new_item)
-
-total_cost = 0.0
-for item in items:
-    total_cost+=(item.item_price * item.item_quantity)
-    item.print_item_cost()
-print(f"Total: ${total_cost:.2f}")
