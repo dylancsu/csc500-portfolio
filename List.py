a1aae35bd57395a0e7595c14a475c38ef3285cfb
dylancsu <dylan.dean@csuglobal.edu>
2024-08-25 18:41:33 -0600

    Add methods

--- List.py
+++ List.py
@@ -1,6 +1,7 @@
 class ItemToPurchase:
     def __init__(self, **kwargs):
         self.item_name=kwargs.get('item_name','none')
+        self.item_description=kwargs.get('item_description','none')
         self.item_price=kwargs.get('item_price',0.0)
         self.item_quantity=kwargs.get('item_quantity',0)
     def print_item_cost(self):
--- ShoppingCart.py
+++ ShoppingCart.py
@@ -5,6 +5,23 @@ class ShoppingCart:
         self.customer_name=kwargs.get('customer_name','none')
         self.current_date=kwargs.get('current_date','none')
         self.cart_items=[]
-    def add_items(ItemToPurchase item):
+    def add_items(item):
         self.cart_items.append(item)
-        
+    def remove_item(item_name):
+        for item in self.cart_items:
+            if item.item_name==item_name:
+                self.cart_items.remove(item)
+                break
+        print("Item not found in cart. Nothing removed.")
+    def modify_item(new_item):
+        for item in self.cart_items:
+            if item.item_name==new_item.item_name:
+                if new_item.item_quantity!=0:
+                    item.item_quantity=new_item.item_quantity
+                if new_item.item_price!=0.0:
+                    item.item_price=new_item.item_price
+                if new_item.item_description!='none':
+                    item.item_description=new_item.item_description
+                break
+            print("Item not found in cart. Nothing modified.")
+            
