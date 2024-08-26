4cf46e252f1de9a0ed4f5d46bacaaf6b67fc7750
dylancsu <dylan.dean@csuglobal.edu>
2024-08-25 18:33:09 -0600

    Constructor

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,1 +1,10 @@
 from List import ItemToPurchase
+
+class ShoppingCart:
+    def __init__(self, **kwargs):
+        self.customer_name=kwargs.get('customer_name','none')
+        self.current_date=kwargs.get('current_date','none')
+        self.cart_items=[]
+    def add_items(ItemToPurchase item):
+        self.cart_items.append(item)
+        
